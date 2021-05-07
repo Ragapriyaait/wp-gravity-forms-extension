2c61f9ce1b9fbf604811f76cdb47ac1a03774811
Ragapriyaait <75374788+Ragapriyaait@users.noreply.github.com>
2021-05-07 19:04:15 +0530

    Add files via upload

--- wp-starter.php
+++ wp-starter.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gravity Forms WPMktgEngine Extension
 Description: This plugin requires the WPMKtgEngine or Genoo plugin installed before order to activate.
-Version: 2.2.31
+Version: 2.2.32
 Requires PHP: 7.1
 Author: Genoo LLC
 */
