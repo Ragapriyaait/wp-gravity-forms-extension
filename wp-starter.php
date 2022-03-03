d4fdfcd9312366ccd151978c56df7e5087650c2f
Ragapriyaait <75374788+Ragapriyaait@users.noreply.github.com>
2022-03-03 19:10:02 +0530

    Add files via upload

--- wp-starter.php
+++ wp-starter.php
@@ -2,45 +2,85 @@
 
 /*
 
+
+
 Plugin Name: Gravity Forms WPMktgEngine Extension
+
+
 
 Description: This plugin requires the WPMKtgEngine or Genoo plugin installed before order to activate.
 
+
+
 Version: 2.2.75
+
+
 
 Requires PHP: 7.1
 
+
+
 Author: Genoo LLC
+
+
 
 */
 
 /*
 
+
+
     Copyright 2015  WPMKTENGINE, LLC  (web : http://www.genoo.com/)
+
+
 
     This program is free software; you can redistribute it and/or modify
 
+
+
     it under the terms of the GNU General Public License, version 2, as
+
+
 
     published by the Free Software Foundation.
 
 
 
+
+
+
+
     This program is distributed in the hope that it will be useful,
+
+
 
     but WITHOUT ANY WARRANTY; without even the implied warranty of
 
+
+
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+
 
     GNU General Public License for more details.
 
 
 
+
+
+
+
     You should have received a copy of the GNU General Public License
+
+
 
     along with this program; if not, write to the Free Software
 
+
+
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
 
 */
 
@@ -102,11 +142,19 @@ register_activation_hook(__FILE__, function () {
 
     if ($activate == false && $isGenoo == false) { ?>
 
+
+
   <div class="alert">
+
+
 
 <p style="font-family:Segoe UI;font-size:14px;">This plugin requires the WPMKtgEngine or Genoo plugin installed  order to activate</p>
 
+
+
 </div>
+
+
 
     <?php
     die();
@@ -122,25 +170,47 @@ register_activation_hook(__FILE__, function () {
 
         $sql = "CREATE TABLE {$wpdb->prefix}gf_settings (
 
+
+
             id mediumint(8) unsigned not null auto_increment,
+
+
 
             form_id mediumint(8) unsigned not null,
 
+
+
             is_active tinyint(1),
+
+
 
             select_lead_folder varchar(255),
 
+
+
             select_leadtype  varchar(255),
+
+
 
             select_folder  varchar(255),
 
+
+
             select_email varchar(255),
+
+
 
             select_webinar  varchar(250),
 
+
+
             PRIMARY KEY  (id),
 
+
+
             UNIQUE KEY form_id (form_id)
+
+
 
                   ) $charset_collate;";
 
@@ -149,7 +219,11 @@ register_activation_hook(__FILE__, function () {
 
 /**
 
+
+
  * Plugin Updates
+
+
 
  */
 
@@ -655,19 +729,33 @@ add_action(
             ];
             ?>
 
+
+
     <div>
 
+
+
         <li class="thirdparty_input_setting field_setting">
+
+
 
          <label class="section_label" for="field_admin_label"><?php _e(
              'Genoo/WPMktgEngine Field:'
          ); ?></label>
 
+
+
          <select id="field_thirdparty_input" onchange="SetFieldProperty('thirdPartyInput', this.value);" class="fieldwidth-3" >
+
+
 
             <option value="">Do not map fields</option>
 
+
+
             
+
+
 
              <?php foreach ($customfieldsjson as $customfields):
                  //comparing labels with premapped labels in trim_custom_array
@@ -676,18 +764,30 @@ add_action(
                      !in_array(trim($customfields->label), $pre_mapped_fields)
                  ): ?>
 
+
+
                      <option value="<?php echo $customfields->key; ?>"> <?php echo trim(
     $customfields->label
 ); ?></option> <?php endif;
              endforeach; ?>
 
+
+
        </select>
+
+
 
             </li>
 
+
+
             </div>
 
+
+
               
+
+
 
                     <?php if (method_exists($WPME_API, 'callCustom')):
                         try {
@@ -706,22 +806,38 @@ add_action(
                             elseif ($WPME_API->http->getResponseCode() == 200):
                                 $i = 0; ?>
 
+
+
                                       <div class="leadtypecheckbox" style="height:200px;overflow: auto";>
 
+
+
                                       <h1 class="editheader">Edit Label Here:</h1>
+
+
 
                                     <?php foreach (
                                         $leadtypes_optional
                                         as $leadtypes_optional_values
                                     ) { ?>
 
+
+
                         <li class="encrypt_setting_leadtypes field_setting">
+
+
 
         
 
+
+
                 <input type="checkbox" id="field_encrypt_value<?php echo $i; ?>" name="field_encrypt_value<?php echo $i; ?>" data-id =<?php echo $i; ?> value="<?php echo $leadtypes_optional_values->id; ?>" onchange="SetFieldProperty('encryptField<?php echo $i; ?>', this.value);" />
 
+
+
                 <label for="field_encrypt_value<?php echo $i; ?>" class="leadtype_value_label<?php echo $i; ?>" style="display:inline;">
+
+
 
                     <?php _e(
                         $leadtypes_optional_values->name,
@@ -729,27 +845,51 @@ add_action(
                         'Gravity Forms WPMktgEngine Extension'
                     ); ?>
 
+
+
                     <?php gform_tooltip('form_field_encrypt_value'); ?>
 
+
+
                 </label>  
+
+
 
                 <input type="text" id="field_id_input_label_text" class="field_id_input_label_text<?php echo $i; ?>" value="<?php echo $leadtypes_optional_values->name; ?>" style="display: none;"/>
 
 
 
+
+
+
+
                 <input type="text" id="field_id_input_label_text" class="field_id_input_value_text<?php echo $i; ?>" value="<?php echo $leadtypes_optional_values->id; ?>" style="display: none;"/>
+
+
 
             </li>
 
+
+
                      
+
+
 
                                    <?php $i++;} ?>
 
+
+
                    </div>
+
+
 
                    <div> <button type="button" class="leadtypeselected">submit</button></div>
 
+
+
                    <div> <button type="button" class="leadtypeupdate" style="display: none;">update</button></div>
+
+
 
                                    <?php
                             endif;
@@ -757,16 +897,30 @@ add_action(
                         }
                     endif; ?>
 
-           
+
 
            
 
+
+
            
+
+
+
+           
+
+
 
          <?php
         endif; ?>
 
+
+
          
+
+
+
+
 
 
 
@@ -870,37 +1024,73 @@ add_action('gform_editor_js', function () {
 
     foreach ($all_default_types as $default_type): ?>
 
+
+
     <script type="text/javascript">
 
+
+
     
+
+
 
         var type = '<?php echo $default_type; ?>';
 
+
+
        
+
+
 
      
 
+
+
       fieldSettings[type] += ', .thirdparty_input_setting';
+
+
 
       fieldSettings[type] += ', .encrypt_setting_leadtypes';
 
+
+
       
 
+
+
     
+
+
 
       // Make sure our field gets populated with its saved value
 
+
+
     jQuery(document).on("gform_load_field_settings", function(event, field, form) {
+
+
 
         var leadtypescount = '<?php echo $count; ?>';
 
+
+
     
+
+
 
         var third_party_value = field['thirdPartyInput'];
 
+
+
         
 
+
+
             jQuery("#field_thirdparty_input").val(field["thirdPartyInput"]);
+
+
+
+
 
 
 
@@ -908,25 +1098,51 @@ add_action('gform_editor_js', function () {
 
 
 
+
+
+
+
             
+
+
 
             jQuery("#field_encrypt_value"+i).prop( 'checked', ( rgar( field, 'encryptField'+i )) );
 
 
 
+
+
+
+
             
+
+
 
         }
 
+
+
                 
+
+
 
             if(third_party_value!='leadtypes')
 
+
+
             {
+
+
 
              jQuery('.leadtypecheckbox').css('display','none');   
 
+
+
              jQuery('.leadtypeselected').css('display','none');   
+
+
+
+
 
 
 
@@ -934,24 +1150,40 @@ add_action('gform_editor_js', function () {
 
 
 
-           
+
+
+
 
            
+
+
+
+           
+
+
 
       });
 
+
+
      
+
+
 
         //binding to the load field settings event to initialize the checkbox
 
 
 
+
+
+
+
     </script>
+
+
 
    <?php endforeach;
 });
-
-
 
 add_action('plugins_loaded', 'myplugin_update_db_check');
 
@@ -1163,7 +1395,6 @@ function log_form_deleted($form_id)
     endif;
 }
 
-
 add_action('admin_enqueue_scripts', 'adminEnqueueScripts', 10, 1);
 
 add_action('wp_ajax_lead_type_option_submit', 'lead_type_option_submit');
@@ -1232,43 +1463,55 @@ function myplugin_ajaxurl()
 {
     echo '<script type="text/javascript">
 
+
+
                        var ajaxurl = "' .
         admin_url('admin-ajax.php') .
         '";
 
+
+
                      </script>';
 }
 
-// define the upgrader_package_options callback 
-function custom_upgrader_package_options( $options ){ 
-   //custom code here
+// define the upgrader_package_options callback
+
+add_action( 'upgrader_process_complete', 'lead_folder_field_creation_test', 10, 2 );
+
+        function lead_folder_field_creation_test( $upgrader_object, $options ) {
+           
+             global $wpdb;
+             
+             //get plugin file.
+             
+              $our_plugin = plugin_basename( __FILE__ );
+
+             $is_plugin_updated = false;
+             
+             //check plugin is active
+          
+            if ( isset( $options['plugins'] ) && is_array( $options['plugins'] ) ) {
+                foreach ( $options['plugins'] as $index => $plugin ) {
+                    if ($our_plugin === $plugin ) {
+                        $is_plugin_updated = true;
+                        break;
+                    }
+                }
+            }
+        
+            if ( ! $is_plugin_updated ) {
+                return;
+            }
 
    
-    return $options;
-} 
+   update_option('plugin_file_updated','yes'); 
 
-//add the action 
-add_filter('upgrader_package_options', 'custom_upgrader_package_options', 10, 1);
 
-function custom_logsdsdss($message) { 
-    if(is_array($message)) { 
-        $message = json_encode($message); 
-    } 
-    $file = fopen("../custom_logsgf.log","a"); 
-    echo fwrite($file, "\n" . date('Y-m-d h:i:s') . " :: " . $message); 
-    fclose($file); 
-}
+        }
 
-// define the upgrader_pre_download callback 
-function custom_upgrader_pre_download( $false, $package, $instance ){ 
-   //custom code here
-      custom_logsdsdss("cccccccccccccccccccccccccccccc");
-    return $false
-} 
-
-//add the action 
-add_filter('upgrader_pre_download', 'custom_upgrader_pre_download', 10, 3);
 
 require_once 'includes/api-functions.php';
 ?>
+
+
 
